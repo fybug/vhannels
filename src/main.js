8440f4f7006610c93158bfdaf45fb8044fa56fd0
fybug <1006291762@qq.com>
2020-12-01 11:11:45 +0800

    移除测试数据

--- src/main.js
+++
@@ -1,7 +0,0 @@
-var floatmess = new vhannels.floatContlos.FloatMessage();
-var mess = floatmess.createMessage();
-var mess2 = floatmess.createMessage();
-mess.message_load("qwewq");
-mess2.message_load("asdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdfasdasdf");
-floatmess.showMessage(mess, 2000);
-setTimeout(() => floatmess.showMessage(mess2), 1000);
