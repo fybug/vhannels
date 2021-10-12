b109e2ca3544f22e22af73669a51e21fcfb3a6bb
fybug <1006291762@qq.com>
2021-10-12 17:23:57 +0800

    追加控件与说明

--- PDWebpack/run.js
+++ PDWebpack/run.js
@@ -2,9 +2,13 @@ confload.runOf('ModeLoad', /** @param {ModeLoad} modeload */(modeload) => {
     // modeload.addMode('app', SourcePath + 'main.js', "defer");
 
     modeload.addMode('vhannels_publiclib', SourcePath + 'vhannels/main.js');
+    // 浮动控件包
     modeload.addMode('vhannels.floatContlos_all', SourcePath + 'vhannels/floatContlos/main.js');
     modeload.addMode('vhannels.floatContlos.floatmessage', SourcePath + 'vhannels/floatContlos/floatmessage/main.js');
     modeload.addMode('vhannels.floatContlos.floatdialog', SourcePath + 'vhannels/floatContlos/floatdialog/main.js');
+    // 页面控件包
+    modeload.addMode('vhannels.pageContlos_all', SourcePath + 'vhannels/pageContlos/main.js');
+    modeload.addMode('vhannels.pageContlos.tab', SourcePath + 'vhannels/pageContlos/tab/main.js');
     // 工具包
     modeload.addMode('vhannels.tool', SourcePath + 'vhannels/tool/main.js');
 
